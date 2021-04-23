cf5f175047f41788f91ae7fd6e4ae51e52d7233d
Joao Guerra <joao.pedro.guerra@hotmail.com>
2021-04-23 14:22:57 +0100

    api test

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const logger       = require('morgan');
 const path         = require('path');
 
 
+
 mongoose
   .connect('mongodb://localhost/mdb', {useNewUrlParser: true})
   .then(x => {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router  = express.Router();
+const imdb = require('imdb-api')
 
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
+  let results = await imdb.search({
+    name: 'MAtrix'
+  }, {
+    apiKey: process.env.IMDB_KEY
+  })
+  
+  console.log(results)
   res.render('index');
+
 });
 
 module.exports = router;
